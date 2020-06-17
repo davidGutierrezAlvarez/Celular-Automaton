74608531c2eecf0c282bcdb5d09e3038261955a9
David Gutierrez Alvarez <david_xd1996@live.com.mx>
2020-06-17 18:22:58 -0500

    Se agrego la clase Colors, nos servira para tomar cada pincel para pintar, el fondo de nuetro mapa cambio a color negro, se agrego el tipo de celula WireWorld

---
+++ CelularAutomaton/Colors.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: David Gutierrez
+ * Date: 17/06/2020
+ * Time: 05:28 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+
+namespace CelularAutomaton {
+
+	public class Color {
+		static public Pen penBlack = new Pen(Color.Black);//lapiz negro
+		static public Pen penWhite = new Pen(Color.White);//lapiz blanco
+		
+		static public Brush brushBlack = new SolidBrush(Color.Black);//lapiz negro
+		static public Brush brushWhite = new SolidBrush(Color.White);//lapiz blanco
+		static public Brush brushRed	 = new SolidBrush(Color.FromArgb(255, 64, 0));//lapiz negro
+		static public Brush brushBlue = new SolidBrush(Color.FromArgb(0, 128, 255));//lapiz negro
+		static public Brush brushBlueLight = new SolidBrush(Color.FromArgb(0, 211, 242));//lapiz negro
+		static public Brush brushGold = new SolidBrush(Color.Gold);//lapiz negro
+		
+		public Color() { }
+	}
+}
